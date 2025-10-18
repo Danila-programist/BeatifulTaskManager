b546a7cda9ca1e572244ce8e6b769fae742eee8a
Danila Golubev <danila25golubev@gmail.com>
2025-10-18 14:45:13 +0300

    feat: init core for config with class Settings

---
+++ backend/app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_CONTAINER_NAME: str
+    DB_PORT: int
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
