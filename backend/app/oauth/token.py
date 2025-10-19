9211e012644c24ea33fef5517282c2e0ad0b7ff0
Danila Golubev <danila25golubev@gmail.com>
2025-10-19 19:41:24 +0300

    feat: add class ClientJWT for create tokens

---
+++ backend/app/oauth/token.py
@@ -0,0 +1,20 @@
+import datetime
+from typing import Dict, Optional
+
+import jwt 
+
+from app.core import settings
+
+class ClientJWT:
+    def __init__(self, data: Optional[Dict[str, str]]):
+        self.data = data
+
+    def create_token(self):
+        payload: Dict = {}
+        payload.update(self.data)
+        payload["exp"] = datetime.datetime.now() + datetime.timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+        return jwt.encode(
+            payload=payload, key=settings.SECRET, algorithm=settings.ALGORYTHM
+        )
