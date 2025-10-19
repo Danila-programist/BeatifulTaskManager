f0be0df92f7b3d8eacefefa2a0fbc13e1da7e196
Danila Golubev <danila25golubev@gmail.com>
2025-10-19 23:01:37 +0300

    chore: change name of oauth

--- backend/app/api/endpoints/user.py
+++ backend/app/api/endpoints/user.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter
 
 
-oauth_router = APIRouter(prefix="/auth", tags=['oauth'])
+auth_router = APIRouter(prefix="/auth", tags=['oauth'])
 
 
-@oauth_router.post("/register", summary="Регистрация нового пользователя")
-async def register(): ...
+@auth_router.post("/register", summary="Регистрация нового пользователя")
+async def register(): 
+    ...
 
 
-@oauth_router.post("/login", summary="Авторизация пользователя")
+
+@auth_router.post("/login", summary="Авторизация пользователя")
 async def login(): ...
 
 
-@oauth_router.post("/logout", summary="Выход пользователя из аккаунта")
+@auth_router.post("/logout", summary="Выход пользователя из аккаунта")
 async def logout(): ...
