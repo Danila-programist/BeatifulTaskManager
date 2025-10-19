27baf96ca674ba23b32dacdefb6c4ef52d515dac
Danila Golubev <danila25golubev@gmail.com>
2025-10-19 20:22:31 +0300

    fix: changes for connect

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
-from api.endpoints import analytics_router, task_router, oauth_router
+from app.api.endpoints import analytics_router, task_router, oauth_router
 
 
 router = APIRouter(prefix="/api/v1")
-router.include_router(analytics_router, task_router, oauth_router)
+router.include_router(analytics_router)
+router.include_router(task_router)
+router.include_router(oauth_router)
 
 
 __all__ = ["router"]
