c365cc801efb6c0f45f26bc642380057eccab0ac
Danila Golubev <danila25golubev@gmail.com>
2025-10-19 20:16:58 +0300

    style: change by black

--- backend/app/api/endpoints/analytics.py
+++ backend/app/api/endpoints/analytics.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
 
 
-
 analytics_router = APIRouter()
 
 
 @analytics_router.get(path="/analytics")
-async def analytics_endpoint():
-    ...
+async def analytics_endpoint(): ...
--- backend/app/api/endpoints/user.py
+++ backend/app/api/endpoints/user.py
@@ -1,20 +1,16 @@
 from fastapi import APIRouter
 
 
-
-oauth_router = APIRouter(prefix='/auth')
-
-
-@oauth_router.post('/register', summary="Регистрация нового пользователя")
-async def register():
-    ...
+oauth_router = APIRouter(prefix="/auth")
 
 
-@oauth_router.post('/login', summary='Авторизация пользователя')
-async def login():
-    ...
+@oauth_router.post("/register", summary="Регистрация нового пользователя")
+async def register(): ...
 
 
-@oauth_router.post('/logout', summary="Выход пользователя из аккаунта")
-async def logout():
-    ...
+@oauth_router.post("/login", summary="Авторизация пользователя")
+async def login(): ...
+
+
+@oauth_router.post("/logout", summary="Выход пользователя из аккаунта")
+async def logout(): ...
