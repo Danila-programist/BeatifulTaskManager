b3aca9e92c645c7a85023cdc0f8255aea5f63693
Danila Golubev <danila25golubev@gmail.com>
2025-10-19 19:45:05 +0300

    feat: add new variable for config class Settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,12 +5,19 @@ from pydantic import PostgresDsn
 
 
 class Settings(BaseSettings):
+    # Database 
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_CONTAINER_NAME: str
     DB_PORT: int
+
+    # Hashing
+    PWD_ALGORYTHM: str
+    ALGORYTHM: str
+    SECRET: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), "..", "..", "..", ".env")
