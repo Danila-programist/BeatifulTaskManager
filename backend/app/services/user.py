eb61795aa9bf402ade114ead0ae29d19a0e10fb5
Danila Golubev <danila25golubev@gmail.com>
2025-10-21 22:10:59 +0300

    feat: create new method add new user

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -5,10 +5,12 @@ from sqlalchemy import select
 
 from app.api.schemas import DatabaseUser, RegisterUser
 from app.models import User
+from app.utils import pwd_manager
 
-class UserModel:
+class UserService:
     def __init__(self, db: AsyncSession):
         self._session = db
+        self._pwd_manager = pwd_manager
 
     async def get_user(self, username: str) -> Optional[DatabaseUser]:
         stmt = select(User).where(User.username == username)
@@ -16,7 +18,17 @@ class UserModel:
         return res.scalar_one_or_none()
     
     async def add_new_user(self, user: RegisterUser) -> None:
-        ...
+        new_user: User = User(
+            username=user.username,
+            email=user.email,
+            password_hash=self._pwd_manager.hash_password(user.password),
+            first_name=user.first_name,
+            last_name=user.last_name,
+        ) 
+
+        self._session.add(new_user)
+        await self._session.commit()
+        await self._session.refresh(new_user)
 
 
 
