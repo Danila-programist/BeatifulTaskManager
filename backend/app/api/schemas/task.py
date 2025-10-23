a13c5b093bc3f5082e3acfb90fea6218fe68a5e3
Danila Golubev <danila25golubev@gmail.com>
2025-10-23 10:44:31 +0300

    feat: add new schemas for task

---
+++ backend/app/api/schemas/task.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from datetime import datetime
+from uuid import UUID
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class TaskStasus(str, Enum):
+    PENDING = "pending"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+
+
+class BaseTask(BaseModel):
+    title: str = Field(..., max_length=256)
+    description: Optional[str] = None
+    status: TaskStasus = TaskStasus.PENDING
+
+
+class TaskRequest(BaseTask):
+    pass
+
+
+class TaskResponse(BaseTask):
+    task_id: int
+    created_at: datetime
+    updated_at: datetime
+    is_active: bool
+    user_id: UUID
