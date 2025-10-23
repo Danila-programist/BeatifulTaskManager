74ca3da26071a3df4566ae5ff7a854008c089842
Danila Golubev <danila25golubev@gmail.com>
2025-10-23 10:46:39 +0300

    feat: add schemas in __init__

--- backend/app/api/schemas/__init__.py
+++ backend/app/api/schemas/__init__.py
@@ -1,4 +1,14 @@
 from .user import LoginUser, RegisterUser, DatabaseUser, BaseUser
+from .task import TaskRequest, TaskStatus, BaseTask, TaskResponse
 
 
-__all__ = ["LoginUser", "RegisterUser", "DatabaseUser", "BaseUser"]
+__all__ = [
+    "LoginUser",
+    "RegisterUser",
+    "DatabaseUser",
+    "BaseUser",
+    "TaskRequest",
+    "TaskStatus",
+    "BaseTask",
+    "TaskResponse",
+]
