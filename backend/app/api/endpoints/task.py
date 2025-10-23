3d9ad4270233b2669ce0fabe5be295ee6ed52217
Danila Golubev <danila25golubev@gmail.com>
2025-10-23 12:38:44 +0300

    feat: add new endpoint and models for create task

--- backend/app/api/endpoints/task.py
+++ backend/app/api/endpoints/task.py
@@ -1,21 +1,34 @@
-from fastapi import APIRouter, Depends
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.auth import get_current_user
 from app.db import get_db
 from app.services import TaskService
-from app.api.schemas import DatabaseUser
+from app.api.schemas import DatabaseUser, TaskResponse, TaskRequest
 
 task_router = APIRouter(tags=["tasks"])
 
 
 @task_router.get("/tasks")
-async def get_all_tasks(current_user: DatabaseUser = Depends(get_current_user), db: AsyncSession = Depends(get_db)): 
+async def get_all_tasks(current_user: DatabaseUser = Depends(get_current_user), db: AsyncSession = Depends(get_db)) -> List[TaskResponse]: 
     task_service = TaskService(db, current_user.username)
+    tasks = await task_service.get_user_tasks()
+    return [TaskResponse.model_validate(task) for task in tasks]
 
 @task_router.post("/tasks")
-async def create_new_task(current_user: str = Depends(get_current_user), db: AsyncSession = Depends(get_db)): ...
-
+async def create_new_task(task_req: TaskRequest, current_user: str = Depends(get_current_user), db: AsyncSession = Depends(get_db)): 
+    task_service = TaskService(db, current_user.username)
+    task = await task_service.create_task(task_req)
+    
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Task was not created"
+        )
+    
+    return {"message": 'Task was added'}
 
 @task_router.get("/tasks/{task_id}")
 async def get_task_by_id(current_user: str = Depends(get_current_user), db: AsyncSession = Depends(get_db)): ...
--- backend/app/services/task.py
+++ backend/app/services/task.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, and_
 
 from app.models import Task, User
+from app.api.schemas import TaskRequest
 
 
 class TaskService:
@@ -31,3 +32,24 @@ class TaskService:
         )
         res = await self._session.execute(stmt)
         return res.scalars().all()
+
+
+    async def create_task(self, task_data: TaskRequest) -> Optional[Task]:
+        user_id = await self._get_user_id()
+        
+        if not user_id:
+            return None
+
+        new_task = Task(
+            title=task_data.title,
+            description=task_data.description,
+            status=task_data.status,
+            user_id=user_id
+        )
+
+        self._session.add(new_task)
+        await self._session.commit()
+        await self._session.refresh(new_task)
+        return new_task
+
+   
